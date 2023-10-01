0a433a946b8461961a00126af473674021401ad1
Piêtro <pietrobragaprofissional@gmail.com>
2023-10-01 15:33:23 -0300

    Circuit Breaker + Feign com fallback method

--- src/main/java/com/pietrobaj/feign_java_21/feignClients/RandomUserClient.java
+++ src/main/java/com/pietrobaj/feign_java_21/feignClients/RandomUserClient.java
@@ -1,12 +1,19 @@
 package com.pietrobaj.feign_java_21.feignClients;
 
 import com.pietrobaj.feign_java_21.models.RandomUserResult;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "RandomUserClient", url="https://randomuser.me/api")
+@FeignClient(name = "RandomUserClient", url = "https://randomuser.me/api")
 public interface RandomUserClient {
+    @CircuitBreaker(name = "randomUserApi", fallbackMethod = "fallbackGetUsers")
     @RequestMapping(method = RequestMethod.GET, value = "/")
     RandomUserResult getUsers();
+
+    default RandomUserResult fallbackGetUsers(Exception ex) {
+        return new RandomUserResult();
+    }
+
 }
